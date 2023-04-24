58ff9025734294b499767e021621b9edfc50cd0e
TranVanDuy2003 <tranduypro2003@gmail.com>
2023-04-24 20:45:00 +0700

    19-04

--- BatThucHanhLab2/BaiThucHanhLab3/src/NhanVien/MainNhanVien.java
+++
@@ -1,9 +0,0 @@
-public class MainNhanVien {
-    public static void main(String[] args) {
-        NhanVien nv= new NhanVien();
-        nv.nhaptt();
-        nv.tinhluong();
-        nv.intt();
-    }
-    
-}
